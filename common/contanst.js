ef4bd283b0fe9bff6579846f784a8c067f87e26e
slash214 <yangjianping@glorigin.tech>
2024-10-12 10:50:36 +0800

    feat page

---
+++ common/contanst.js
@@ -0,0 +1,4 @@
+// 用户协议
+export const UserAgreement = "https://api.haoxianhui.com/static/userAgreement.html"
+// 隐私政策
+export const PrivacyPolicy = "https://api.haoxianhui.com/static/privacyPolicy.html"
--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,26 +1,53 @@
 <template>
-	<view>
-		<button @click="jump">跳转</button>
+	<view class="login flex-column flex-between flex-items-center">
+		<view class="login-header flex-column flex-items-center">
+			<text>好闲惠</text>
+			<text>引领新零售时代</text>
+		</view>
+		
+		<view class="login-box">
+			21122112
+		</view>
+		
+		<view class="login-footer">
+			<view class="">
+				我已经阅读并同意<text><<用户协议>><text/>和<text><<隐私政策>></text>
+			</view>
+			<u--text text="好闲惠掌柜宝"></u--text>
+			<u--text text="门店加盟：400-690-8890"></u--text>
+		</view>
+
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		},
-		methods: {
-			jump() {
-				uni.switchTab({
-					url: '/pages/overview/overview',
-				})
-			}
+export default {
+	data() {
+		return {}
+	},
+	methods: {
+		jump() {
+			uni.switchTab({
+				url: '/pages/overview/overview'
+			})
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-
+.login {
+	width: 100vw;
+	height: 100vh;
+	overflow: hidden;
+	background: linear-gradient(to bottom right, #77d7c3, #3a92da);
+	&-header {
+		margin-top: 300rpx;
+		font-size: 56rpx;
+		color: #fff;
+		font-weight: 400;
+		letter-spacing: 2px;
+		line-height: 1.5;
+	}
+}
 </style>
